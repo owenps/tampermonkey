0b2cc0ac4a974056230a8e4188569d616a0ed0ea
Owen <owensmith321@gmail.com>
2026-01-03 12:19:37 -0500

    Update author field in metadata

--- github-conventional-comments.user.js
+++ github-conventional-comments.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.1.0
 // @description  Add conventional comments buttons to GitHub PR reviews and issues
-// @author       @owenps
+// @author       owenps
 // @match        https://github.com/*
 // @grant        GM_getValue
 // @grant        GM_setValue
